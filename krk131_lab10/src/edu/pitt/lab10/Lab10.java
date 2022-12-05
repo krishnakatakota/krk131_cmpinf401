2d09ee35d551fc73c981252f6c9d5ecaceecdac4
quadkopter <krishnakanth.katakota@gmail.com>
2022-12-05 18:36:10 -0500

    Lab 10

---
+++ krk131_lab10/src/edu/pitt/lab10/Lab10.java
@@ -0,0 +1,40 @@
+package edu.pitt.lab10;
+
+public class Lab10 {
+
+	public static void main(String[] args) {
+		System.out.println(sumOfInts(11));
+		System.out.println(sumOfInts(0));
+		System.out.println(sumOfInts(9));
+		System.out.println(sumOfInts(91368)); // Should return 27
+		System.out.println("\n\n");
+		
+		int[] a = {2,5,3,30,753,23124};
+		printArrayElements(a, 0);
+		
+	}
+	
+	public static int sumOfInts(int num) {
+		int x = Math.abs(num);
+		
+		if (x / 10 == 0) {
+			return x;
+		}
+		else {
+			return (x % 10) + sumOfInts(x / 10);
+		}
+		
+	}
+	
+	public static void printArrayElements(int arr[], int i) {
+		
+		if(arr.length - 1 == i) {
+			System.out.println(arr[0]);
+		}
+		else {
+			System.out.println(arr[i]);
+			printArrayElements(arr, i + 1);
+		}
+		
+	}
+}
